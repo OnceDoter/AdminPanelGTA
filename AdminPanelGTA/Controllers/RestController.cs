c16f5a0fc37c37b700f83235af1993f7975c0452
CausalEwe <ser42rus@gmail.com>
2021-09-04 17:39:46 +0700

    Добавлен метод GetPlayers

--- AdminPanelGTA/Controllers/RestController.cs
+++ AdminPanelGTA/Controllers/RestController.cs
@@ -41,6 +41,17 @@ namespace AdminPanelGTA.Controllers
 		{
 			IQueryable<Player> players = _db.Players;
 			if (request.Name != null) players = players.Where(p => p.Name.Contains(request.Name));
+			if (request.Title != null) players = players.Where(p => p.Title.Contains(request.Title));
+			if (request.Race != null) players = players.Where(p => p.Race.Contains(request.Race));
+			if (request.Profession != null) players = players.Where(p => p.Profession.Contains(request.Profession));
+			if (request.MinExperience > 0) players = players.Where(p => p.Experience >= request.MinExperience);
+			if (request.MaxExperience > 0) players = players.Where(p => p.Experience <= request.MaxExperience);
+			if (request.MinLevel > 0) players = players.Where(p => p.Level >= request.MinLevel);
+			if (request.MaxLevel > 0) players = players.Where(p => p.Level <= request.MaxLevel);
+			if (request.Banned == true) players = players.Where(p => p.Banned == true);
+			else players = players.Where(p => p.Banned == false);
+			if (request.After > 0) players = players.Where(p => p.Birthday.Year >= request.After);
+			if (request.Before > 0) players = players.Where(p => p.Birthday.Year < request.Before);
 			return new JsonResult(players);
 		}
 
